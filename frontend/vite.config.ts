4e339d2b43571cb08f48f83ddf420dbdedc2cf97
trickster144 <ashleybrand@msn.com>
2026-04-12 08:30:13 +0100

    1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: '10.0.0.28:10011',
         changeOrigin: true
       }
     }
